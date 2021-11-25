33206ee91382031f0162f446584d100f5fd88fa6
danbai <danbai@88.com>
2021-11-25 22:37:16 +0800

    优化代码结构 命令行历史

---
+++ src/main/kotlin/com/github/danbai225/pwlchat/utils/StringUtils.kt
@@ -0,0 +1,26 @@
+package com.github.danbai225.pwlchat.utils
+
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+object StringUtils {
+    fun  md5(input: String?): String? {
+        if (input == null || input.length == 0) {
+            return null
+        }
+        try {
+            val md5 = MessageDigest.getInstance("MD5")
+            md5.update(input.toByteArray())
+            val byteArray = md5.digest()
+            val sb = StringBuilder()
+            for (b in byteArray) {
+                // 一个byte格式化成两位的16进制，不足两位高位补零
+                sb.append(String.format("%02x", b))
+            }
+            return sb.toString().replace("\\s".toRegex(), "").toLowerCase()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+}
